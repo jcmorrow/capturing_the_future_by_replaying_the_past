d1f85496d24a11a428e17edc6fcbc01846b222b8
Josh Morrow <josh@jcmorrow.com>
2020-08-03 16:03:27 -0700

    Add comments

--- simple_continuation.rb
+++ simple_continuation.rb
@@ -6,20 +6,27 @@ end
 
 puts 'What is your name?'
 name = gets
+# => Josh
 
 continuation = capture_continuation do
   puts "Hello #{name}"
 end
 
-puts 'See, I can do whatever I want with this now. For example, I could call it twice:'
+puts(
+  'See, I can do whatever I want with this. For example, I could call it twice:'
+)
 
 continuation.call
+# => Hello Josh
 continuation.call
+# => Hello Josh
 
 puts 'or I could redefine puts:'
 
 def puts(string)
-  print("#{string.bytes.join(' ')}\n")
+  print(string.chars.join(' '))
 end
 
 continuation.call
+# => 72 101 108 108 111 32 74 111 115 104
+#    H  e   l   l   o      J  o   s   h
